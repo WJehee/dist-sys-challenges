55109fdca7549ea93463695e66ac6fd3e45e195e
Wouter Jehee <wouterjehee@tutanota.com>
2023-10-21 11:33:39 +0200

    add function for handling initial message

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,6 +1,25 @@
+use std::io::{BufRead, Write};
+
+use Dist_sys_challenges::{Message, HandleMessage, Init, Body, handle_init};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum Type {
+    Echo { echo: String },
+    EchoOk {echo: String},
+}
+
+type EchoMessage = Message<Type>;
 
 fn main() {
-    // TODO: read msg from std::in into struct
+    let stdin = std::io::stdin().lock();
+    let mut stdout = std::io::stdout().lock();
+
+    handle_init(stdin, stdout);
+    
     // TODO: process it
     // TODO: write msg to std::out
 }
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,55 @@
+use std::io::{BufRead, Write};
+
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
-pub struct Message {
-    src: String,
-    dest: String,
-    body: Body,
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Message<T> {
+    pub src: String,
+    #[serde(rename = "dest")]
+    pub dst: String,
+    pub body: Body<T>,
 }
 
-#[derive(Deserialize, Serialize)]
-pub struct Body {
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Body<T> {
     #[serde(flatten)]
-    msg_type: Type,
-    msg_id: Option<usize>,
-    in_reply_to: Option<usize>,
+    pub msg_type: T,
+    pub msg_id: Option<usize>,
+    pub in_reply_to: Option<usize>,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
-pub enum Type {
-    Echo { echo: String },
-    EchoOk {echo: String},
+pub enum Init {
+    Init { node_id: String, node_ids: Vec<String> },
+    InitOk,
+}
+
+pub fn handle_init(stdin: impl BufRead, mut stdout: impl Write) {
+    let mut stdin = stdin.lines();
+
+    let init_msg: Message<Init>= serde_json::from_str(
+        &stdin.next()
+            .unwrap()
+            .expect("failed to read line")
+    ).expect("unable to parse message");
+
+    let node_id = match init_msg.body.msg_type {
+        Init::Init{node_id, node_ids: _} => node_id,
+        _ => panic!("did not receive init message first"),
+    };
+
+    let init_reply = Message {
+        src: node_id,
+        dst: init_msg.src,
+        body: Body {
+            msg_id: Some(0),
+            in_reply_to: init_msg.body.msg_id,
+            msg_type: Init::InitOk,
+        }
+    };
+    serde_json::to_writer(&mut stdout, &init_reply).unwrap();
+    stdout.write_all(b"\n").unwrap();
 }
 
